d8eb5ef4dbff71d6bf7bb55fea0fa75fc9bbe735
Samuel Batissou <samuelbatissou@gmail.com>
2020-01-30 20:20:17 +0100

    added working traits, and tests!

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+mod fst;
+mod snd;
+mod trd;
+
+pub use fst::Fst;
+pub use snd::Snd;
+pub use trd::Trd;
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{Fst, Snd};
+
+    #[test]
+    fn fst() {
+        let x = vec![(2, 3), (3, 4), (4, 5), (5, 6)];
+
+        let expected = vec![2, 3, 4, 5];
+        assert_eq!(x.iter().map(Fst::fst).cloned().collect::<Vec<_>>(), expected);
+        assert_eq!(x.into_iter().map(Fst::into_fst).collect::<Vec<_>>(), expected);
+    }
+
+    #[test]
+    fn snd() {
+        let x = vec![(2, 3), (3, 4), (4, 5), (5, 6)];
+
+        let expected = vec![3, 4, 5, 6];
+        assert_eq!(x.iter().map(Snd::snd).cloned().collect::<Vec<_>>(), expected);
+        assert_eq!(x.into_iter().map(Snd::into_snd).collect::<Vec<_>>(), expected);
+    }
+}
